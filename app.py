178ecfcdf009af375aede95f710b80db95c6bc2f
eugenesm <eugenesm@ronis.mail>
2020-08-28 23:20:18 +0300

    update

--- app.py
+++ app.py
@@ -1,3 +1,26 @@
-#https://habr.com/ru/post/448310/
-#pip install pytelegrambotapi
-import telebot
+from telebot import TeleBot
+import random
+
+app = TeleBot(__name__)
+
+@app.route('(?!/).+')
+def parrot(message):
+   chat_dest = message['chat']['id']
+   user_msg = message['text']
+
+   #msg = "Parrot Says: {}".format(user_msg)
+   messages = [
+       'м',
+       'Владка классная',
+       'Ну конечно',
+       'Всё понятно',
+       '😘',
+       'Ой ну не ной',
+       'Это да',
+       'Умничка'
+   ]
+   app.send_message(chat_dest, random.choice(messages))
+
+if __name__ == '__main__':
+    app.config['api_key'] = '1336768592:AAEL3X6W-Eo5DaHtQ78U6YFokTBk3MZFQp0'
+    app.poll(debug=True)
